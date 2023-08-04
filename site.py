eb1165a27684858ef0c0cd10558995b08842f0d5
Ben Chamblee <64371981+Bench-amblee@users.noreply.github.com>
2023-08-04 08:28:36 -0500

    Update site.py

--- site.py
+++ site.py
@@ -37,7 +37,12 @@ def similar_artist(artist_choice,number_input):
     test_df['Arist'] = ra_df['name']
     test_df['Similarity Score'] = ra_df['match']
 
+    for i in range(len(test_df):
+        button_text = test_df['Artist'][i]
+        x = st.button(button_text)
+    
     return test_df
+    return x
 
 
 st.title('Last.FM Similar Artists Generator')
