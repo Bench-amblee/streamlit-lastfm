ff2d16ef02454cfb6fc6e860d941038fa8b1d4cf
Ben Chamblee <64371981+Bench-amblee@users.noreply.github.com>
2023-08-03 21:14:38 -0500

    Update site.py

--- site.py
+++ site.py
@@ -8,7 +8,7 @@ connection = LastFMConnector()
 examples = ['Radiohead', 'Daft Punk', 'Weezer', 'Porter Robinson', 'The Weeknd', 'Kali Uchis', 'Custom']
 artist_input = st.selectbox('Select An Artist', examples, index=0)
 
-if artist_input = 'Custom':
+if artist_input == 'Custom':
   custom = st.text_input('Choose a musical artist (Case Sensitive)')
   artist_input = custom
 
