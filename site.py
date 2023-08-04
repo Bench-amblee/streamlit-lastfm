6c47b20506053d24698ab903a96cd1639baeb474
Ben Chamblee <64371981+Bench-amblee@users.noreply.github.com>
2023-08-03 21:16:10 -0500

    Update site.py

--- site.py
+++ site.py
@@ -5,12 +5,14 @@ import pandas as pd
 st.title('Last.FM Similar Artists Generator')
 connection = LastFMConnector() 
 
-examples = ['Radiohead', 'Daft Punk', 'Weezer', 'Porter Robinson', 'The Weeknd', 'Kali Uchis', 'Custom']
+examples = ['Taylor Swift','Radiohead', 'Daft Punk', 'Weezer', 'Porter Robinson', 'The Weeknd', 'Kali Uchis', 'Custom']
 artist_input = st.selectbox('Select An Artist', examples, index=0)
 
 if artist_input == 'Custom':
   custom = st.text_input('Choose a musical artist (Case Sensitive)')
   artist_input = custom
+
+similar_artists = st.slider('How many similar artists would you like?',1,5)
 
 
 
