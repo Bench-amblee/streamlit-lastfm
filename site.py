18ad991423501337d47f86932f3c352bf4152e36
Ben Chamblee <64371981+Bench-amblee@users.noreply.github.com>
2023-08-03 21:10:47 -0500

    Update site.py

--- site.py
+++ site.py
@@ -6,5 +6,5 @@ st.title('Last.FM Similar Artists Generator')
 connection = LastFMConnector() 
 
 examples = ['Radiohead', 'Daft Punk', 'Weezer', 'Porter Robinson', 'The Weeknd', 'Kali Uchis']
-artist_input = st.selectbox('Select An Artist', options, index=0,)
+artist_input = st.selectbox('Select An Artist', options, index=0)
 
